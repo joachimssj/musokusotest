f23aa15d5ed658b904840f577897d160ce50c6a9
joachim yazigi <joachim.yazigi@gmail.com>
2020-12-15 22:37:58 +0100

    save

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import { Helmet } from 'react-helmet'
+import logo from './logo.svg';
+import './App.css';
+
+import videoHD from './musokuso_comingsoon.mp4';
+import videoMobile from './musokuso_comingsoon_Mobile.mp4';
+import loadingImage from './loadingImg.jpeg';
+import loadingImageMobile from './loadingImg_mobile.jpeg'
+
+function App() {
+    const [isMuted, setIsMuted] = useState(true);
+    const [isMutedMobile, setIsMutedMobile] = useState(true);
+
+    return (
+    <div className="App">
+        <Helmet>
+            <title>{ '𝗠𝗨𝗦𝗢 𝗞𝗨𝗦𝗢®' }</title>
+        </Helmet>
+        <div className="bg-image bg-image__HD">
+            <img className="bg-image__content" src={loadingImage} alt="musokuso"/>
+        </div>
+        <div className="bg-image bg-image__LD">
+            <img className="bg-image__content" src={loadingImageMobile} alt="musokuso"/>
+        </div>
+        <div className={"bg-video bg-video__HD"}>
+            <video className={'bg-video__content'} id={"video"} onClick={() => setIsMuted(!isMuted)} autoPlay={true} muted={isMuted} loop={true} playsinline={true} playsInline={true}>
+                <source src={videoHD} type="video/mp4"/>
+                Your browser is not supported
+            </video>
+        </div>
+        <div className="bg-video bg-video__LD">
+            <video className="bg-video__content" onClick={() => setIsMutedMobile(!isMutedMobile)} id="video2" autoPlay={true} muted={isMutedMobile} loop={true} playsinline={true} playsInline={true}>
+                <source src={videoMobile} type="video/mp4"/>
+                    Your browser is not supported
+            </video>
+        </div>
+    </div>
+  );
+}
+
+export default App;
